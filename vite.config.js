74205577885a4585b90f6c13a053d0db119beb8f
THE EVEN <96623398+The3ven@users.noreply.github.com>
2025-02-16 00:19:23 +0530

    Update vite.config.js
    Updated using chrome on android

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+   host: "0.0.0.0",
+   port: Number(process.env.PORT) || 5173,
+   strictPort: true,
+   cors: true,
+   allowed Hosts: ['my-react-app-xljg.onrender.com],
+ }
 })
