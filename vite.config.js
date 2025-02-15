87dac015a6f982a03b6f37a5bfc7e37d53eeb86b
THE EVEN <96623398+The3ven@users.noreply.github.com>
2025-02-16 00:37:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-   cors: true,
-   allowedHosts: ['my-react-app-xljg.onrender.com'],
- }
+  server.allowedHosts: "my-react-app-xljg.onrender.com",
 })
