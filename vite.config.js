5c8a37d26f25bdc44d1074b3e7abf6c62a5cb2fd
sazid <sazid@qboid.io>
2025-02-16 07:12:59 +0530

    Add right host now
    Signed-off-by: sazid <sazid@qboid.io>

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["my-react-app-xljg.onrender.com"]
+    allowedHosts: ["my-react-app-x1jg.onrender.com"]
   }
 })
